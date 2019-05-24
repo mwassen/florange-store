d09532ccc251ef04651d02c4183e6cf53cb42400
Marcus W <marcuswassen@gmail.com>
2019-05-24 16:08:25 +0200

    Updates

---
+++ precache-manifest.9b87a04e2e3e49c28f631c1030323b4c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2172b9ff63b3f4ca222f2904744a368d",
+    "url": "/florange-store/index.html"
+  },
+  {
+    "revision": "3d37f61803d787485ae1",
+    "url": "/florange-store/static/css/main.a0dac672.chunk.css"
+  },
+  {
+    "revision": "cf7cd88c57700d3b43ef",
+    "url": "/florange-store/static/js/2.e7f4d7eb.chunk.js"
+  },
+  {
+    "revision": "3d37f61803d787485ae1",
+    "url": "/florange-store/static/js/main.4edcc0d5.chunk.js"
+  },
+  {
+    "revision": "74a8dae88be80a6c7e04",
+    "url": "/florange-store/static/js/runtime~main.9de3f624.js"
+  },
+  {
+    "revision": "04958462932102c5f6a05d455683a26a",
+    "url": "/florange-store/static/media/logo.04958462.png"
+  },
+  {
+    "revision": "332dd5d3366a4ab78249538d09693b93",
+    "url": "/florange-store/static/media/poolbg.332dd5d3.png"
+  }
+]);
