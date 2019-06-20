0899310d5631b0179766b6b641344bad241f20e2
Marcus W <marcuswassen@gmail.com>
2019-06-20 19:44:53 +0200

    Updates

---
+++ precache-manifest.d3636f380927830b710ef7646823b380.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89003f2854a50cb6e43cbcde56b17289",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d77058f2ed7e3c08d6bb",
+    "url": "/static/css/2.ddaa9116.chunk.css"
+  },
+  {
+    "revision": "fe47cb778261fe51b387",
+    "url": "/static/css/main.eb14b7c8.chunk.css"
+  },
+  {
+    "revision": "d77058f2ed7e3c08d6bb",
+    "url": "/static/js/2.fe1341e8.chunk.js"
+  },
+  {
+    "revision": "fe47cb778261fe51b387",
+    "url": "/static/js/main.a952784d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "04958462932102c5f6a05d455683a26a",
+    "url": "/static/media/logo.04958462.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "9428f160fb0436bab55117af58d54dbf",
+    "url": "/static/media/waterbg.9428f160.mp4"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/florange-store/precache-manifest.5fd90d40fe939be356e23cfc288c0273.js"
+  "/precache-manifest.d3636f380927830b710ef7646823b380.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/florange-store/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
